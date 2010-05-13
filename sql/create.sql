a3a4ff7867a7ac3d04e593a13d964b0ba1b16f81
Gavin <tops@gavinmogan.com>
2010-05-13 11:04:34 -0700

    adding some basic sql data

---
+++ sql/create.sql
@@ -0,0 +1,1 @@
+create table eventTypes (id int primary key auto_increment, name varchar(255), nameKey varchar(255), textColor varchar(10), bgColor varchar(10));
---
+++ sql/insert.sql
@@ -0,0 +1,6 @@
+insert into eventTypes VALUES 
+	(NULL, 'Performance', 'performance', '#111111', '#FF7F47'),
+	(NULL, 'Game / Contests', 'game_contests', '#3A3939;', '#75FF62'),
+	(NULL, 'Art / Creative', 'art_creative', '#000000', '#017CFD'),
+	(NULL, 'Academic', 'academic', '#000000', '#FB0000'),
+	(NULL, 'Jap. Culture', 'jap_culture', '#000000', '#9B9B9B');
