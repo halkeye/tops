20a1e3e9c9a37c56977c1bd822b26dbf2a148a67
Gavin <halkeye@gmail.com>
2010-06-07 00:01:51 -0700

    adding key from other urls

--- admin/config/authorizedUsers.php
+++ admin/config/authorizedUsers.php
@@ -1,5 +1,9 @@
 <?php
 defined('TOPS_PAGE') or die("not a valid hit");
 $authorizedUsers = array(
-        //'https://www.google.com/accounts/o8/id?id=AItOawnUan1HD0rXuzWv9RGMmnvexPqxbCfJCAo' => 1,
+        // moocow one
+        'https://www.google.com/accounts/o8/id?id=AItOawnUan1HD0rXuzWv9RGMmnvexPqxbCfJCAo' => 1,
+        // moocow.halkeye.net one
+        'https://www.google.com/accounts/o8/id?id=AItOawn1f8u3zHHp4sOyItEhmzLbfFlgZo6pEPA' => 1,
+
 );
