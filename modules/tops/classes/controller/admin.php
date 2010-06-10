e66fce6712242d624b664ee9b21edc15c5324cb2
Gavin <halkeye@gmail.com>
2010-06-09 21:21:16 -0700

    working on getting more of openid working

--- modules/tops/classes/controller/admin.php
+++ modules/tops/classes/controller/admin.php
@@ -2,16 +2,17 @@
 
 class controller_admin extends Controller_Template 
 {
-	public $template = 'adminTemplate';
+    public $template = 'adminTemplate';
     protected $session;
     
     function before()
     {
+        $ret = parent::before();
+
         $this->session = Session::instance();
         $this->requireAuth();
 
         #$this->requireAdmin();
-		$ret = parent::before();
         $this->template->currentPage = str_replace(array('Edit', 'Create', 'Save'), '', $this->request->action);
         $this->template->title = "index";
         $this->template->account = new StdClass;
@@ -23,28 +24,28 @@ class controller_admin extends Controller_Template
 
     function action_index()
     {
-		$this->template->content = 'hello, world!';
+        $this->template->content = 'hello, world!';
     }
     function action_rooms()
     {
-		$this->template->content = 'hello, world!';
+        $this->template->content = 'hello, world!';
     }
     function action_days()
     {
-		$this->template->content = 'hello, world!';
+        $this->template->content = 'hello, world!';
     }
     function action_types()
     {
-		$this->template->content = 'hello, world!';
+        $this->template->content = 'hello, world!';
     }
 
     function requireAuth()
     {
         if (!$this->session->get('account_id'))
         {
-        #    $this->session->set('redirected_from', url::current());
-        #    url::redirect('openid/tryAuth');
-        #    exit();
+            $this->session->set('redirected_from', $this->request->uri);
+            $this->request->redirect('openid/tryAuth');
+            return false;
         }
         $email = $this->session->get('account_email');
         $email = 'halkeye@gmail.com';
@@ -55,8 +56,8 @@ class controller_admin extends Controller_Template
             return TRUE;
         }
 
-        die(url::redirect('system/accessDenied'));
-        exit();
+        $this->request->redirect('system/accessDenied')l;
+        return false;
     }
 
 }
