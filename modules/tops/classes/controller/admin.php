21119c05eab072b5dc427ac342b4f8b799e2d44f
Gavin <halkeye@gmail.com>
2010-06-09 17:53:45 -0700

    email access list

--- modules/tops/classes/controller/admin.php
+++ modules/tops/classes/controller/admin.php
@@ -3,9 +3,13 @@
 class controller_admin extends Controller_Template 
 {
 	public $template = 'adminTemplate';
+    protected $session;
     
     function before()
     {
+        $this->session = Session::instance();
+        $this->requireAuth();
+
         #$this->requireAdmin();
 		$ret = parent::before();
         $this->template->currentPage = str_replace(array('Edit', 'Create', 'Save'), '', $this->request->action);
@@ -32,6 +36,27 @@ class controller_admin extends Controller_Template
     function action_types()
     {
 		$this->template->content = 'hello, world!';
+    }
+
+    function requireAuth()
+    {
+        if (!$this->session->get('account_id'))
+        {
+        #    $this->session->set('redirected_from', url::current());
+        #    url::redirect('openid/tryAuth');
+        #    exit();
+        }
+        $email = $this->session->get('account_email');
+        $email = 'halkeye@gmail.com';
+
+        $emails = Kohana::config("googleAuth.emails");
+        if (isset($emails[$email]) && $emails[$email])
+        {
+            return TRUE;
+        }
+
+        die(url::redirect('system/accessDenied'));
+        exit();
     }
 
 }
