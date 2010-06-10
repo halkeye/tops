35338acb6793e6d949636edcbab9da90b2aa8c18
Gavin <halkeye@gmail.com>
2010-06-10 13:56:21 -0700

    schedule controller

--- modules/tops/classes/controller/schedule.php
+++ modules/tops/classes/controller/schedule.php
@@ -4,6 +4,17 @@ class controller_schedule extends Controller_Template
 {
     function action_index()
     {
-		$this->template->content = View::factory('schedule/index');
+        $this->request->title = "Schedule Page";
+        require_once(dirname(__FILE__).'/../../../../_inc/include.data.php'); 
+		$this->template->content = View::factory('schedule/index',array(
+                    'eventTypes' => $eventTypes,
+                    'borderWidth' => $borderWidth,
+                    'days' => $days,
+                    'rooms' => $rooms,
+                    'hourData' => $hourData,
+                    'hourHeight' => $hourHeight,
+                    'data' => $data,
+        ));
+
     }
 }
--- modules/tops/views/schedule/index.php
+++ modules/tops/views/schedule/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<?php include_once('_inc/include.data.php'); ?>
 <html>
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
