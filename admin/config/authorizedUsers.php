d1774ead89cf044231f6644229b2d68cf2039fe5
Gavin <halkeye@gmail.com>
2010-06-09 18:33:22 -0700

    removing files

--- admin/config/authorizedUsers.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TOPS_PAGE') or die("not a valid hit");
-$authorizedUsers = array(
-        // moocow one
-        'https://www.google.com/accounts/o8/id?id=AItOawnUan1HD0rXuzWv9RGMmnvexPqxbCfJCAo' => 1,
-        // moocow.halkeye.net one
-        'https://www.google.com/accounts/o8/id?id=AItOawn1f8u3zHHp4sOyItEhmzLbfFlgZo6pEPA' => 1,
-
-);
