15e716bd013d1c581fd083f873486138d613edf0
Gavin <github@gavinmogan.com>
2010-07-24 12:35:39 -0700

    import randy(systemr)'s design

--- modules/tops/views/js/main.js
+++ modules/tops/views/js/main.js
@@ -1,28 +1,37 @@
-var firstClick = 1;
-function clearAllOnFirstClick(){
-    if (!firstClick) {
-        return;
-    }
-    firstClick = 0;
-    
-    jQuery("#schedItemSelector td").each(function(a, obj){
-        var className = jQuery(obj).attr('class');
-        jQuery('.schedItem.' + className).addClass(className + 'Offline').removeClass(className);
-    });
-}
-
 jQuery(document).ready(function(){
-    jQuery("#schedItemSelector td").toggle(function(){
-        clearAllOnFirstClick();
-        var obj = jQuery(this);
-        obj.addClass('selectedSchedButton');
-        var className = obj.attr('class').replace(' selectedSchedButton', '');
-        jQuery('.schedItem.' + className + 'Offline').removeClass(className + 'Offline').addClass(className);
-    }, function(){
-        var obj = jQuery(this);
-        obj.removeClass('selectedSchedButton');
-        var className = obj.attr('class').replace(' selectedSchedButton', '');
-        jQuery('.schedItem.' + className).addClass(className + 'Offline').removeClass(className);
-    });
-    
+    Scheduler.init();
 });
+
+var Scheduler = function() {
+    var firstClick, $schedItemSelectors, $scheduleContent;
+    function clearAllOnFirstClick(){
+        firstClick = 0;
+        for(var i = $schedItemSelectors.length; i--; ) {
+            var className = $schedItemSelectors.eq(i).attr('class');
+            $scheduleContent.find('.schedItem.' + className).addClass(className + 'Offline').removeClass(className);
+        }
+    } 
+    return {
+        init: function() {
+            firstClick = 1;
+            $schedItemSelectors = jQuery("#schedItemSelector").find("div");
+            $scheduleContent = jQuery("#scheduleContent");
+            
+            //Set toggle
+            $schedItemSelectors.toggle(function(){
+                if (firstClick) {
+                    clearAllOnFirstClick();
+                }
+                var obj = jQuery(this);
+                obj.addClass('selectedSchedButton');
+                var className = obj.attr('class').replace(' selectedSchedButton', '');
+                $scheduleContent.find('.schedItem.' + className + 'Offline').removeClass(className + 'Offline').addClass(className);
+            }, function(){
+                var obj = jQuery(this);
+                obj.removeClass('selectedSchedButton');
+                var className = obj.attr('class').replace(' selectedSchedButton', '');
+                $scheduleContent.find('.schedItem.' + className).addClass(className + 'Offline').removeClass(className);
+            });
+        }
+    };
+}();
--- modules/tops/views/schedule/index.php
+++ modules/tops/views/schedule/index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
     <title>Schedule</title>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <?php echo HTML::script('static/js/jquery.js', NULL, TRUE); ?>
     <?php echo HTML::script('static/js/main.js', NULL, TRUE); ?>
     <?php echo HTML::style('static/css/schedule.css', NULL, TRUE); ?>
+    <link rel="stylesheet" href="css/style.css" />
     <style text="text/css">
+        <?php
+        /*
+         *  It's best to use the following color (Pantone colors):
+         *  #FCBBC4, #C4E3A1, #ADCFE6, #99DBDE, #D1A3CC, #F5ED59, #F0C49E
+         */
+        ?>
         <?php foreach ($eventTypes as $eventType): ?>
             .<?php echo htmlentities($eventType['nameKey']) ?> { 
                 color: <?php echo htmlentities($eventType['textColor']) ?>;
@@ -19,62 +26,74 @@
     </style>
 </head>
 <body>
-    <?php foreach ($days as $day): ?>
-        <h3><?php echo date('l jS F Y', $day); ?></h3><br />
-        <?php $totalHours = (($hourData[$day]['max']-$hourData[$day]['min'])/100); ?>
-    
-        <div style="margin-left: 25px">
-            <?php foreach ($rooms as $room): ?>
-                <div class="small roomHeader"><?php echo htmlentities($room) ?></div>
-            <?php endforeach ?>
-        </div>
-        <div class="clear"></div>
-   
-        <?php foreach ($rooms as $room) 
-        { ?>
-            <div style="position: relative; float: left; margin-bottom: 25px">
-                <div class="roomBlock" style="height: <?php echo (($totalHours+1)*$hourHeight)-2 ?>px; <?php if ($room == $rooms[0]): ?> margin-left: 50px<?php endif ?>">
-                <?php 
-                    $timeOffset = 0;
-                    foreach ($data[$room][$day] as $hour => $eventData) 
-                    {
-                        $timeHeight = floor($hourHeight/2)*$eventData['length']-12 /* 12 = padding + border */;
-                        $timeOffset = -1;
-                        if ($hour != $hourData[$day]['min'])
-                            $timeOffset = $hourHeight*floor(($hour-$hourData[$day]['min'])/100);
-                        if ($timeOffset > 0) $timeOffset -= 1;
-                        ?>
-                        <div style="margin-top: <?php echo $timeOffset; ?>px">
-                            <div style="height: <?php echo $timeHeight; ?>px;" class="schedItem <?php echo implode(' ', $eventData['type']) ?>">
-                                <?php echo htmlentities($eventData['name']); ?>
-                            </div>
-                        </div>
-                    <?php
-                    } 
-                ?>
-               </div>
-                
-                <?php if ($room == $rooms[0]): ?>
-                    <div class="timeBlock" style="height: <?php echo (($totalHours+1)*$hourHeight) ?>px">
-                        <?php $count=0; foreach (range($hourData[$day]['minHour'], $hourData[$day]['maxHour']) as $hour): ?>
-                        <div class="timeHeader" style="margin-top: <?php echo $hourHeight*$count++ ?>px;">
-                            <?php echo date('g:i A', mktime($hour,0)); ?>
-                        </div>
-                        <?php endforeach ?>
-                    </div>
-                <?php endif; ?>
-            </div>
-        <?php 
-        }
+    <div class="container">
+        <?php
+            //Calculate total width for content
+            $totalWidth = 51; //50px + 1px border 
+	        foreach ($rooms as $room) {
+	        	$totalWidth += 151; //150px + 1px border
+	        } 
         ?>
-    <?php endforeach; ?>
-    <br style="clear:both" />
-    <table id="schedItemSelector">
-        <tr>
-            <?php foreach ($eventTypes as $eventType): ?>
-                <td class="<?php echo htmlentities($eventType['nameKey']) ?>"><?php echo htmlentities($eventType['name']) ?></td>
-        <?php endforeach ?>
-        </tr>
-    </table>
+        <div id="scheduleContent" class="content" style="width: <?php echo $totalWidth ?>px">
+		    <?php foreach ($days as $day): ?>
+		        <h3><?php echo date('l jS F Y', $day); ?></h3><br />
+		        <?php $totalHours = (($hourData[$day]['max']-$hourData[$day]['min'])/100); ?>
+		    
+		        <div style="margin-left: 25px">
+		            <?php foreach ($rooms as $room): ?>
+		                <div class="small roomHeader"><?php echo htmlentities($room) ?></div>
+		            <?php endforeach ?>
+		        </div>
+		        <div class="clear"></div>
+		        <?php $lastRoom = $rooms[count($rooms)-1]; ?>
+		        <?php foreach ($rooms as $room) 
+		        { ?>
+		            <div class="roomColumn">
+		                <div class="roomBlock" style="height: <?php echo (($totalHours+1)*$hourHeight)-2 ?>px; <?php if ($room == $rooms[0]): ?> margin-left: 50px <?php elseif ($room == $lastRoom): ?> border-right: 1px solid #838383 <?php endif ?>">
+		                <?php 
+		                    $timeOffset = 0;
+		                    foreach ($data[$room][$day] as $hour => $eventData) 
+		                    {
+		                        $timeHeight = floor($hourHeight/2)*$eventData['length']-13 /* 12 = padding + border */;
+		                        $timeOffset = 0;
+		                        if ($hour != $hourData[$day]['min'])
+		                            $timeOffset = $hourHeight*floor(($hour-$hourData[$day]['min'])/100);
+		                        ?>
+		                        <div style="margin-top: <?php echo $timeOffset; ?>px">
+		                            <div style="height: <?php echo $timeHeight; ?>px;" class="schedItem <?php echo implode(' ', $eventData['type']) ?>">
+		                                <?php echo htmlentities($eventData['name']); ?>
+		                            </div>
+		                        </div>
+		                    <?php
+		                    } 
+		                ?>
+		               </div>
+		                
+		                <?php if ($room == $rooms[0]): ?>
+		                    <div class="timeBlock" style="height: <?php echo (($totalHours+1)*$hourHeight) ?>px">
+		                        <?php $count=0; foreach (range($hourData[$day]['minHour'], $hourData[$day]['maxHour']) as $hour): ?>
+		                        <div class="timeHeader" style="margin-top: <?php echo $hourHeight*$count++ ?>px;">
+		                            <?php echo date('g:i A', mktime($hour,0)); ?>
+		                        </div>
+		                        <?php endforeach ?>
+		                    </div>
+		                <?php endif; ?>
+		            </div>
+		        <?php 
+		        }
+		        ?>
+		    <?php endforeach; ?>
+		    <br style="clear:both" />
+	    </div>
+    </div>
+    <div class="legend">
+        <table id="schedItemSelector">
+            <tr>
+                <?php foreach ($eventTypes as $eventType): ?>
+                    <td><div class="<?php echo htmlentities($eventType['nameKey']) ?>"><?php echo htmlentities($eventType['name']) ?></div></td>
+                <?php endforeach ?>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
